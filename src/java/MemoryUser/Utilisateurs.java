85e285a18ed823e7651788699afdedc5829689ee
Boris Klett <boris.klett@he-arc.ch>
2016-04-06 13:25:59 +0200

    loging midified

--- src/java/MemoryUser/Utilisateurs.java
+++ src/java/MemoryUser/Utilisateurs.java
@@ -4,28 +4,32 @@
  */
 package MemoryUser;
 
+import dao.UtilisateurDao;
 import java.util.Hashtable;
+import java.util.List;
+import modele.Utilisateur;
 
 /**
  *
  * @author francill
  */
 public class Utilisateurs {
-    public static Hashtable<String,String>  users =null;
 
-    public static boolean verifyUser(String user, String pw){
-        if(users == null){
-            users = new Hashtable<String,String>();
-            users.put("termine", "ptermine");
-            users.put("daucourt","pdaucourt");
-            users.put("francillon","pfrancillon");
-            users.put("dupont","pdupont");
-            users.put("t","t");
-        }     
-        System.out.println(user + " " + pw +" " +users.get(user));
-        if(users.get(user)!=null){
-            if ( users.get(user).equals(pw) ) return true;else return false;
-        }else return false;
-        
+    public static List users = UtilisateurDao.researchAll();
+
+    public static boolean verifyUser(String user, String pw) {
+        Utilisateur util = new Utilisateur();
+        if (!UtilisateurDao.researchByUsername(user).isEmpty()) {
+            util = UtilisateurDao.researchByUsername(user).get(0);
+            if (util.getPwd().equals(pw)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
     }
+
 }
--- src/java/dao/UtilisateurDao.java
+++ src/java/dao/UtilisateurDao.java
@@ -18,26 +18,26 @@ import modele.Utilisateur;
  * @author silvio.gutierre
  */
 public class UtilisateurDao {
-       
-    public static ArrayList<Utilisateur> researchAll(){
+
+    public static ArrayList<Utilisateur> researchAll() {
         ArrayList<Utilisateur> users = new ArrayList<Utilisateur>();
-        
+
         Connection cnx = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-        
+
         try {
             cnx = OracleConnections.getConnection();
 
             String query = "select numero, username, pwd from employe";
-
-            rs = pstmt.executeQuery(query);
+            pstmt = cnx.prepareStatement(query);
+            rs = pstmt.executeQuery();
 
             while (rs.next()) {
                 Integer numero = rs.getInt("numero");
                 String username = rs.getString("username");
                 String pwd = rs.getString("pwd");
-                Utilisateur user = new Utilisateur(numero,username,pwd);
+                Utilisateur user = new Utilisateur(numero, username, pwd);
                 users.add(user);
             }
             return users;
@@ -55,41 +55,37 @@ public class UtilisateurDao {
 
         }
     }
-    
-    public static ArrayList<Utilisateur> researchByUsername(String username){
+
+    public static ArrayList<Utilisateur> researchByUsername(String username) {
         ArrayList<Utilisateur> users = new ArrayList<Utilisateur>();
-        
+
         Connection cnx = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-        
+
         try {
             cnx = OracleConnections.getConnection();
 
             String query = "select numero, username, pwd from employe where username = ?";
-            pstmt.setString(1, username);            
-            rs = pstmt.executeQuery(query);
+            pstmt = cnx.prepareStatement(query);
+            pstmt.setString(1, username);
+            rs = pstmt.executeQuery();
 
             while (rs.next()) {
                 Integer numero = rs.getInt("numero");
                 String pwd = rs.getString("pwd");
                 String user_name = rs.getString("username");
-                Utilisateur user = new Utilisateur(numero,user_name,pwd);
+                Utilisateur user = new Utilisateur(numero, user_name, pwd);
                 users.add(user);
             }
+            rs.close();
+            pstmt.close();
+            cnx.close();
             return users;
+
         } catch (SQLException ex) {
             System.out.println("Error SELECT CONNECTION: " + ex.getMessage());
             return null;
-        } finally {
-            try {
-                rs.close();
-                pstmt.close();
-                cnx.close();
-            } catch (SQLException ex) {
-                System.out.println("Error SELECT SQL: " + ex.getMessage());
-            }
-
         }
     }
 }
